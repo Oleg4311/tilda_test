b0443aece827b1fb1bb732a23d758dfa356c18c3
Oleg <olegbelii4311@gmail.com>
2022-10-13 08:02:49 +0300

    rating_without_saving

--- project/js/app.js
+++ project/js/app.js
@@ -1,19 +1,19 @@
 const input = document.querySelector('.input');
 const button = document.querySelector('.button');
 
-function set_rate(event, el) {
+const myFunc = function set_rate(event, el) {
   // Позиция клика относительно элемента
   event = event || window.event;
   if (event.clientX <= el.clientWidth) {
-    const rate = Math.ceil(event.clientX / el.clientWidth * 5);
-    document.getElementById('rate').innerHTML = `Ваш рейтинг: ${rate}`;
+    const rate = Math.ceil((event.clientX / el.clientWidth) * 5);
+    document.getElementById('rate').innerHTML = `Ваша оценка: ${rate}`;
     el.getElementsByTagName('div')[0].style.width = `${rate * 20}%`;
     if (rate < 4) {
-      input.style.display = 'flex';
+      input.style.display = 'block';
       button.style.display = 'block';
     } else {
       input.style.display = 'none';
       button.style.display = 'none';
     }
   }
-}
+};
